9add6dc538a9c6627d169423ad4674c15ccfb330
jgomeze4 <jgomeze4@eafit.edu.co>
2019-11-03 18:24:44 -0500

    Arreglar componentes

---
+++ src/app/models/insumo-model.ts
@@ -0,0 +1,14 @@
+export class Insumo{
+    ID_PRODUCTO:number;
+    ID_FAMILIA:number;
+    Nombre:string;
+    Marca:string;
+    Proveedor:string;
+    Codigo:string;
+    Presentacion:string;
+    RegINVIMA:string;
+    ClasificacionRiesgo:string;
+    TipoAlmacenamiento:string;
+    ID_USUARIO:number;
+    Activo:boolean;
+}
---
+++ src/app/models/inventario-model.ts
@@ -0,0 +1,10 @@
+export class Inventario{
+    ID_PRODUCTO:number;
+    Lote:string;
+    FechaVencimiento:Date;
+    Cantidad:number;
+    CantidadAnterior:number;
+    ID_BODEGA:number;
+    ID_USUARIO:number;
+    ID_AUDITORIA:number;
+}
