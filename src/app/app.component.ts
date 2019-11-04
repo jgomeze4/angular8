b56578dbdafd1cd6ca937b6d651e34e57d910a99
jgomeze4 <jgomeze4@eafit.edu.co>
2019-11-04 02:52:02 -0500

    Cambio de titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello World';
+  title = 'LABX';
   InsScreen = false;
   InvScreen = false;
 }
