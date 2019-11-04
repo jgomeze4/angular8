45f1a4c08df9ea2f9d3a2c398b6870837c29c0ae
jgomeze4 <jgomeze4@eafit.edu.co>
2019-11-03 19:31:46 -0500

    Definir el datatable

--- src/app/insumo/listar-ins/listar-ins.component.ts
+++ src/app/insumo/listar-ins/listar-ins.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-listar-ins',
@@ -8,7 +9,8 @@ import { Component, OnInit } from '@angular/core';
 export class ListarInsComponent implements OnInit {
 
   constructor() { }
-
+  listData:MatTableDataSource<any>;
+  displayedColumns: string[] = ['Options','ID_PRODUCTO','ID_FAMILIA','Nombre','Marca','Proveedor','Codigo','Presentacion','RegINVIMA','ClasificacionRiesgo','TipoAlmacenamiento','ID_USUARIO','Activo']
   ngOnInit() {
   }
 
