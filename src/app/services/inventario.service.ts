7aecbf41c256c7fafd27297b4d79bb382ec30579
jgomeze4 <jgomeze4@eafit.edu.co>
2019-11-10 13:29:40 -0500

    Listar Inventario

--- src/app/services/inventario.service.ts
+++ src/app/services/inventario.service.ts
@@ -5,6 +5,7 @@ import {Observable} from 'rxjs';
 import {CookieService} from 'ngx-cookie-service';
 import * as moment from 'moment';
 import { InventarioUpdate } from '../models/inventarioUpdate-model';
+import { InventarioList } from '../models/inventarioList-model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +17,11 @@ export class InventarioService {
   uformData: InventarioUpdate;
 
   readonly APIUrl="http://138.197.0.136:8080/api/inventario";
-  getInventarioList():Observable<Inventario[]>{
+  getInventarioList():Observable<InventarioList[]>{
     var _data = atob(this.cookieService.get('session'));
     let id:string = _data.split(';')[0];
     let token:string = 'Bearer '+_data.split(';')[1];
-    return this.http.get<Inventario[]>(this.APIUrl +"/listar",{headers:{'Content-Type':'application/json','Authorization':token,'id':id}});
+    return this.http.get<InventarioList[]>(this.APIUrl +"/listar",{headers:{'Content-Type':'application/json','Authorization':token,'id':id}});
   }
   addInventario(inv:Inventario){
     var _data = atob(this.cookieService.get('session'));
